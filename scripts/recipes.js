36bc5479d6b0bfb2ea858d20432ea4187e98c2e5
Neo <neogoldahlgren@gmail.com>
2025-10-15 15:25:41 +0200

    woopsie

--- scripts/recipes.js
+++ scripts/recipes.js
@@ -1,7 +1,7 @@
 async function getRecipes(ingred) {
     let req = new Request(`https://api.api-ninjas.com/v2/recipe?title="${ingred}"`, {
         method: "GET",
-        headers: {"X-Api-Key": "zthS6Z89qGOqf5wdjglxug==A0CO1dTYHw8PpEmr"}
+        headers: {"X-Api-Key": ""}
     });
 
     let resp = await fetch(req);
